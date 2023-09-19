ae99ae06d8561b65d26a4180c5a7c27d28effe65
adem kok <ademk7604@gmail.com>
2023-09-19 22:50:29 +0200

    PojoClass

--- src/d15_PojoClass/Calisan.java
+++ src/d15_PojoClass/Calisan.java
@@ -3,28 +3,33 @@ package d15_PojoClass;
 import java.util.Date;
 
 public class Calisan {
-	
+
 	private String name;
 	private double maas;
 	private Date iseGirisTarihiDate;
-	
+
 	public void setName(String s) {
-		name=s;
+		name = s;
 	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setMaas(double d) {
-		maas=d;
+		maas = d;
 	}
-	
+
 	public double getMaas() {
 		return maas;
 	}
-	
+
 	public void setIseGirisTarihi(Date d) {
-		iseGirisTarihiDate=d;
+		iseGirisTarihiDate = d;
+	}
+
+	public Date getIseGirisTarihi() {
+		return iseGirisTarihiDate;
 	}
 
 }
--- src/d15_PojoClass/Driver.java
+++ src/d15_PojoClass/Driver.java
@@ -1,8 +1,19 @@
 package d15_PojoClass;
 
+import java.util.Date;
+
 public class Driver {
 	
 	public static void main(String[] args) {
+		
+		Calisan calisan = new Calisan();
+		calisan.setName("Ali Can");
+		calisan.setMaas(2200);
+		calisan.setIseGirisTarihi(new Date());
+		
+		System.out.println(calisan.getName());
+		System.out.println(calisan.getMaas());
+		System.out.println(calisan.getIseGirisTarihi());
 		
 	}
 
