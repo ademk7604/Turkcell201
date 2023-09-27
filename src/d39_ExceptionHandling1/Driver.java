29d84d74c75f3fcf44ab51e8078233754ef2f28c
adem kok <ademk7604@gmail.com>
2023-09-27 22:44:10 +0200

    ExceptionHandling

---
+++ src/d39_ExceptionHandling1/Driver.java
@@ -0,0 +1,36 @@
+package d39_ExceptionHandling1;
+
+import java.util.Scanner;
+
+public class Driver {
+	public static void main(String[] args) {
+
+		// arithExcep();
+		// arrayExcep();
+		nullPointer();
+
+	}
+
+	public static void nullPointer() {
+		Scanner scanner = null;
+		String line = scanner.nextLine();
+		System.out.println(line); // java.lang.NullPointerException:
+
+	}
+
+	public static void arrayExcep() {
+		int[] numbers = { 0, 1, 2, 3, 4 };
+		Scanner scanner = new Scanner(System.in);
+		int index = scanner.nextInt(); // 5 = java.lang.ArrayIndexOutOfBoundsException
+		System.out.println(numbers[index]);
+	}
+
+	public static void arithExcep() {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt(); // y=0 java.lang.ArithmeticException:
+
+		System.out.println(x / y);
+	}
+
+}
