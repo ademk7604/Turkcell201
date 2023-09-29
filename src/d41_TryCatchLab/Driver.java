302768703494f8b46ee2c482744b57643581fc6a
adem kok <ademk7604@gmail.com>
2023-09-29 06:30:01 +0200

    throw

--- src/d41_TryCatchLab/Driver.java
+++ src/d41_TryCatchLab/Driver.java
@@ -1,21 +1,53 @@
 package d41_TryCatchLab;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Driver {
-	
+
 	public static void main(String[] args) {
-		
-		yaz();
-		
+
+		// yaz();
+		oku();
 	}
-	
+
+	public static void oku() {
+
+		File file = new File("read.me");
+		FileReader fileReader = null;
+		try {
+			fileReader = new FileReader(file);
+			int c = 0;
+			while ((c = fileReader.read()) != -1) { // read() method u int bir deger veriyor. o yuzden ...
+				System.out.print((char) c);
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (fileReader != null) {
+				// "read.text" olarak girdigimde yani yanlis dostya adresi girdigimde null olan
+				// fileReader i kapatmaya calisirken
+				// NullPointer atiyor o yuzden if ile kontrol gerekli
+				try {
+					fileReader.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+
+		}
+
+	}
+
 	public static void yaz() {
 		File file = new File("read.me");
-		FileWriter fileWriter = null;
+		FileWriter fileWriter = null; 
+		// new FileWriter(file); Exception frilattigi icin try in icine aliyoruz. ilk deger null
+
 		Scanner scanner = new Scanner(System.in);
 		try {
 			fileWriter = new FileWriter(file);
@@ -32,7 +64,7 @@ public class Driver {
 			}
 			scanner.close();
 		}
-		
+
 	}
 
 }
